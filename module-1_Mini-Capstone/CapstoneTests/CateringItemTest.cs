8fbc842647703d5c5a4be1606446df435d5b4cfd
Amman Bahta <amman.bahta@gmail.com>
2021-06-05 12:56:23 -0400

    Mini Cap Progress

--- module-1_Mini-Capstone/CapstoneTests/CateringItemTest.cs
+++ module-1_Mini-Capstone/CapstoneTests/CateringItemTest.cs
@@ -2,13 +2,41 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-
+using Capstone.Classes;
 namespace CapstoneTests
 {
     [TestClass]
     public class CateringItemTest
     {
         [TestMethod]
-        public void 
+        public void CateringItem_TypeBReturnsBeverage()
+        {
+            //Arrange
+            CateringItem item = new CateringItem();
+            item.Code = "B5";
+            item.Name = "Jameson";
+            item.Price = 30.00m;
+            item.Type = "B";
+            //Act
+            string result = item.FullType;
+            //Assert
+            Assert.AreEqual("Beverage", result);
+        }
+        [TestMethod]
+        public void CateringItem_TypeFReturnsEmpty()
+        {
+            //Arrange
+            CateringItem item = new CateringItem();
+            item.Code = "B5";
+            item.Name = "Jameson";
+            item.Price = 30.00m;
+            item.Type = "F";
+            //Act
+            string result = item.FullType;
+            //Assert
+            Assert.AreEqual("", result);
+        }
+
     }
+    
 }
--- module-1_Mini-Capstone/CapstoneTests/CateringTest.cs
+++ module-1_Mini-Capstone/CapstoneTests/CateringTest.cs
@@ -1,6 +1,6 @@
 using Capstone.Classes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using System.Collections.Generic;
 namespace CapstoneTests
 {
     [TestClass]
@@ -8,6 +8,28 @@ namespace CapstoneTests
     {
         [TestMethod]
         public void CateringInstanceShouldBeCreated()
+        {
+            // Arrange 
+            Catering catering = new Catering();
+
+            // Act
+
+            // Assert
+            Assert.IsNotNull(catering);
+        }
+        [TestMethod]
+        public void CateringTest_IndexZeroReturnsCode()
+        {
+            // Arrange 
+            Catering catering = new Catering();
+
+            // Act
+            catering.AddItem("A5|Spaghetti|70.20|A");
+            // Assert
+            Assert.AreEqual("A5",catering.FullList[0].Code);
+        }
+        [TestMethod]
+        public void CateringInstanceShouldBeCreate()
         {
             // Arrange 
             Catering catering = new Catering();
